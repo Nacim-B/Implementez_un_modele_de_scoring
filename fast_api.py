7605a5156468326aedfa0633cd2e920b3428bde2
Nacim Berrabah <nacim.berrabah@al-enterprise.com>
2024-12-31 16:29:42 +0100

    v1 api for old client

---
+++ fast_api.py
@@ -0,0 +1,43 @@
+import io
+import pickle
+import pandas as pd
+
+import numpy as np
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# load test data and model
+dataset_path = "./data_test_for_dashboard.csv"
+model_path = "./old_client_model.pkl"
+
+data = pd.read_csv(dataset_path)
+
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
+
+class PredictionRequest(BaseModel):
+    id_client: int
+
+
+# check if
+@app.get("/")
+def read_root():
+    return {"message": "API is working"}
+
+
+@app.post("/predict/")
+def predict(request: PredictionRequest):
+    # verify if client exists
+    if request.id_client not in data["SK_ID_CURR"].values:
+        raise HTTPException(status_code=404, detail="Client ID non trouvé")
+
+    # retrieve client data
+    client_data = data[data["SK_ID_CURR"] == request.id_client].drop(columns=["SK_ID_CURR"])
+
+    # prodiction
+    prediction = model.predict(client_data)[0]
+
+    return {"id_client": request.id_client, "prediction": prediction}
