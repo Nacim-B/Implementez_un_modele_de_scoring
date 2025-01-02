d8c67f603c45dd378e8335f33b000a528fea20ac
Nacim Berrabah <nacim.berrabah@al-enterprise.com>
2025-01-02 17:01:49 +0100

    actual and new clients dashbonard

---
+++ pages/Actual_Clients.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import requests
+
+API_URL = "http://127.0.0.1:8080/predict/"
+
+id_client = st.number_input("Enter client id :", min_value=0, step=1)
+
+threshold_default = 20
+if st.button("Predict"):
+    if id_client:
+        # Appel de l'API
+        try:
+            response = requests.post(API_URL, json={"id_client": id_client})
+
+            if response.status_code == 200:
+                result = response.json()
+                if result['probability'] * 100 <= threshold_default:
+                    st.success(f"Result for client number: {result['id_client']} : Loan Accepted")
+                else:
+                    st.error(f"Result for client number: {result['id_client']} : Loan Declined")
+
+                st.write(f"Certainty of default payment: {round(result['probability'] * 100, 2)}%")
+                st.write(f"Maximum allowed is set at : {threshold_default} %")
+            else:
+                st.error(f"Error: {response.json().get('detail', 'Unknown Problem')}")
+        except requests.exceptions.RequestException as e:
+            st.error(f"Please enter a valid ID")
+    else:
+        st.warning("Please enter a valid ID")
---
+++ pages/New_Clients.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+
+API_URL = "http://127.0.0.1:8080/predict_new_client/"
+
+new_client_data = {'loan_request_amount': st.number_input("Amount Loan Request:", min_value=0,
+                                                          max_value=1000000000, step=5000),
+                   'annual_salary': st.number_input("Annual Income :", min_value=0, max_value=50000000, step=5000),
+                   'annual_annuity': st.number_input("Amount Annuity:", min_value=0, max_value=300000, step=1000),
+                   'age': st.number_input("Age :", min_value=18, max_value=150, step=1) * (-365)
+                   }
+threshold_default = 20
+if st.button("Predict"):
+    try:
+        response = requests.post(API_URL, json=new_client_data)
+
+        if response.status_code == 200:
+            result = response.json()
+            if result['probability'] * 100 <= threshold_default:
+                st.success(f"Result for new client : Loan Accepted")
+            else:
+                st.error(f"Result for new client : Loan Declined")
+
+            st.write(f"Certainty of default payment: {round(result['probability'] * 100, 2)}%")
+            st.write(f"Maximum allowed is set at : {threshold_default} %")
+        else:
+            st.error(f"Error: {response.json().get('detail', 'Unknown Problem')}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Please enter a valid ID")
+
