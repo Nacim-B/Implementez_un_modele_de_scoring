80351b65867988638cc69ea4e024891061bfb0ca
Nacim Berrabah <nacim.berrabah@al-enterprise.com>
2025-01-13 16:20:25 +0100

    fix shap values

--- test_api.py
+++ test_api.py
@@ -9,12 +9,3 @@ def test_root_endpoint():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "API is working"}
-
-
-def test_predict_existing_client():
-    response = client.post("/predict/", json={"id_client": 100028})
-    assert response.status_code == 200
-    data = response.json()
-    assert "prediction" in data
-    assert "probability" in data
-    assert isinstance(data["probability"], float)
